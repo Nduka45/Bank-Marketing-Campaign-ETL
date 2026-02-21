397501e472fa6eeb71ed8392f4bb3ef99525508d
Nduka45 <ndukasunday18@gmail.com>
2026-02-21 04:43:20 +0100

    fixing the main.py code

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
+#import the os and pathlib
 import os
 from pathlib import Path
 
-# Relative imports for src package
 from .visualize import generate_all_plots
 from .transform import (
     load_csv,
@@ -11,7 +11,6 @@ from .transform import (
     clean_economics_data
 )
 
-# Get project root automatically to process the data
 BASE_DIR = Path(__file__).resolve().parent.parent
 RAW_PATH = BASE_DIR / "data" / "raw"
 OUTPUT_PATH = BASE_DIR / "data" / "processed"
@@ -19,28 +18,20 @@ OUTPUT_PATH = BASE_DIR / "data" / "processed"
 def run_pipeline():
     print("Starting ETL pipeline...")
 
-    # Extract
     bank_df = load_csv(RAW_PATH / "bank_marketing.csv")
     campaign_df = load_csv(RAW_PATH / "campaign.csv")
     economics_df = load_csv(RAW_PATH / "economics.csv")
 
     print("Files loaded.")
 
-    # Standardizing each DataFrame for an actual result
     bank_df = clean_columns(bank_df)
     campaign_df = clean_columns(campaign_df)
     economics_df = clean_columns(economics_df)
-    
+
     print("Data cleaned.")
 
-    if "campaign_outcome" not in bank_df.columns:
-    if "subscribed" in bank_df.columns:
-        bank_df.rename(columns={"subscribed": "campaign_outcome"}, inplace=True)
-
-    # Generate all visuals
     generate_all_plots(bank_df)
 
-    # Save outputs
     os.makedirs(OUTPUT_PATH, exist_ok=True)
 
     bank_df.to_csv(OUTPUT_PATH / "bank_cleaned.csv", index=False)
@@ -48,4 +39,7 @@ def run_pipeline():
     economics_df.to_csv(OUTPUT_PATH / "economics_cleaned.csv", index=False)
 
     print("ETL pipeline completed successfully.")
+
+if __name__ == "__main__":
+    run_pipeline()
 
